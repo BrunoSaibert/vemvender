3030582e285bd2a6bff1e5255a3a59ab1edb4a37
kikardo <ricardo.daroc@gmail.com>
2019-04-29 18:28:19 -0300

    correçao de erro merge

--- js/main/formCapturaLeads.js
+++ js/main/formCapturaLeads.js
@@ -1,54 +1,58 @@
+
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyBXZsvtg5Lxx4LLho287lczy_I1OE9-c6M",
-    authDomain: "formulario-c8c1c.firebaseapp.com",
-    databaseURL: "https://formulario-c8c1c.firebaseio.com",
-    projectId: "formulario-c8c1c",
-    storageBucket: "formulario-c8c1c.appspot.com",
-    messagingSenderId: "845763453847"
-  };
-  firebase.initializeApp(config);
-  
-  // Reference messages collection
-  var messagesRef = firebase.database().ref('messages');
-  
-  // Listen for form submit
-  document.getElementById('contactForm').addEventListener('submit', submitForm);
-  
-  // Submit form
-  function submitForm(e){
-    e.preventDefault();
-  
-    // Get values
-    var name = getInputVal('nome');
-    var email = getInputVal('email');
-    
-  
-    // Save message
-    saveMessage(name, email);
-  
-    // // Show alert
-    // document.querySelector('.alert').style.display = 'block';
-  
-    // // Hide alert after 3 seconds
-    // setTimeout(function(){
-    //   document.querySelector('.alert').style.display = 'none';
-    // },3000);
-  
-    // Clear form
-    document.getElementById('contactForm').reset();
-  }
-  
-  // Function to get get form values
-  function getInputVal(id){
-    return document.getElementById(id).value;
-  }
-  
-  // Save message to firebase
-  function saveMessage(name, email){
-    var newMessageRef = messagesRef.push();
-    newMessageRef.set({
-      name: name,
-      email: email
-    });
-  }
+  apiKey: "AIzaSyA3OqJEt5oxJCSOwuWehwZ_Dv6d32x1UHg",
+  authDomain: "formulario-e0ffc.firebaseapp.com",
+  databaseURL: "https://formulario-e0ffc.firebaseio.com",
+  projectId: "formulario-e0ffc",
+  storageBucket: "formulario-e0ffc.appspot.com",
+  messagingSenderId: "220448685518"
+};
+firebase.initializeApp(config);
+
+// Reference messages collection
+var messagesRef = firebase.database().ref('messages');
+
+// Listen for form submit
+document.getElementById('contactForm').addEventListener('submit', submitForm);
+
+// Submit form
+function submitForm(e) {
+  e.preventDefault();
+
+  // Get values
+  var name = getInputVal('nome');
+  var email = getInputVal('email');
+
+  moment.locale('pt-BR');
+  var dataHora = moment().format();
+
+  // Save message
+  saveMessage(name, email, dataHora);
+
+  // // Show alert
+  // document.querySelector('.alert').style.display = 'block';
+
+  // // Hide alert after 3 seconds
+  // setTimeout(function(){
+  //   document.querySelector('.alert').style.display = 'none';
+  // },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+// Function to get get form values
+function getInputVal(id) {
+  return document.getElementById(id).value;
+}
+
+// Save message to firebase
+function saveMessage(name, email, dataHora) {
+  var newMessageRef = messagesRef.push();
+  newMessageRef.set({
+    name: name,
+    email: email,
+    dataHora: dataHora
+  });
+}
